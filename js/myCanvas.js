7e9ed0727bfd3f39ffd7d762626d72d05c0b1d45
Tescostum <tescostum@gmail.com>
2016-01-23 01:30:27 +0900

    Canvasの動的リサイズに対応

--- js/myCanvas.js
+++ js/myCanvas.js
@@ -9,9 +9,15 @@
   
   var circle;
   var stage;
+  
+  var resizeWait = 120;
+  var resizeQueue;
 
   function init() {
     stage = new createjs.Stage("myCanvas");
+    
+    resizeCanvas();
+    setResizeCanvasEvent();
     
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener("tick", stage);
@@ -19,6 +25,26 @@
     drawCircle();
     setLeftButtons();
     setRightButtons();
+  }
+  
+  function setResizeCanvasEvent() {
+    // キューをストック
+    resizeQueue = null;
+    
+    window.addEventListener( 'resize', function(event) {
+      // イベント発生の都度、キューをキャンセル 
+      clearTimeout( resizeQueue );
+    
+      // waitで指定したミリ秒後に所定の処理を実行
+      // 解除できるようにresizeQueueに登録
+      resizeQueue = setTimeout(resizeCanvas, resizeWait);
+    }, false );
+  }
+  
+  function resizeCanvas() {
+      var s = document.getElementById("canvasSection");
+      var w = s.clientWidth;
+      stage.canvas.width = w;
   }
   
   function drawCircle() {
